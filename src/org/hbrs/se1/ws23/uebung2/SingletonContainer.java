d3657570e5e0df6f931fc792ebe0089d1e702c0a
Mark.Sh <shkarin27@mail.ru>
2023-10-28 18:40:12 +0200

    SingletonContainer static gemacht

--- src/org/hbrs/se1/ws23/uebung2/SingletonContainer.java
+++ src/org/hbrs/se1/ws23/uebung2/SingletonContainer.java
@@ -2,13 +2,13 @@ package org.hbrs.se1.ws23.uebung2;
 
 public class SingletonContainer extends Container{
 
-    private SingletonContainer singleton;
+    private static SingletonContainer singleton;
 
     private void SingletonContainer() {
         singleton = (SingletonContainer) new Container();
     }
 
-    public SingletonContainer getInstance() {
+    public static SingletonContainer getInstance() {
         return singleton;
     }
 }
