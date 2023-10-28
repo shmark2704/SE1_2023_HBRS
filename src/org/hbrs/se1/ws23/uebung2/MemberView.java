aa2bbffbeba92e1fad44c71f79ef091496153f0c
Mark.Sh <shkarin27@mail.ru>
2023-10-28 18:26:25 +0200

    Erstellung einer neuen View-Klasse für Betrachtung der Member-Objekte

---
+++ src/org/hbrs/se1/ws23/uebung2/MemberView.java
@@ -0,0 +1,14 @@
+package org.hbrs.se1.ws23.uebung2;
+
+import java.util.List;
+
+public class MemberView {
+
+    public void dump(List<Member> liste) {
+
+    }
+
+    public List<Member> getCurrentList() {
+        return null;
+    }
+}
