90f6a93dbd671b6e2ec40575559768506fce0071
Mark.Sh <shkarin27@mail.ru>
2023-11-03 13:45:36 +0100

    1st draft von ContainerPersistenceTest.java

---
+++ test/org/hbrs/se1/ws23/uebung3/test/ContainerPersistenceTest.java
@@ -0,0 +1,61 @@
+package org.hbrs.se1.ws23.uebung3.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.hbrs.se1.ws23.uebung2.*;
+import org.hbrs.se1.ws23.uebung3.persistence.*;
+
+
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+
+
+public class ContainerPersistenceTest {
+    private Container container = new Container();
+    private PersistenceStrategyStream psS = new PersistenceStrategyStream();
+    private PersistenceStrategyMongoDB psMongoDB = new PersistenceStrategyMongoDB();
+
+    @Test
+    void testStrategySet() {
+        container.setPersistenceStrategy(psS);
+        assertThrows(PersistenceException.class, () -> container.store());
+        assertThrows(PersistenceException.class, () -> container.load());
+    }
+
+    @Test
+    void testMongoDBStrategy() {
+        container.setPersistenceStrategy(psMongoDB);
+        assertThrows(PersistenceException.class, () -> container.store());
+        assertThrows(PersistenceException.class, () -> container.load());
+    }
+
+    @Test
+    void testInvalidStreamLocation() {
+        psS.setLocation("invalid/directory/test");
+        container.setPersistenceStrategy(psS);
+        assertThrows(PersistenceException.class, () -> container.store());
+        assertThrows(PersistenceException.class, () -> container.load());
+    }
+
+    @Test
+    void testRoundTrip() throws ContainerException, PersistenceException {
+        Member member1 = new ConcreteMember(1);
+        Member member2 = new ConcreteMember(2);
+
+        container.addMember(member1);
+        container.addMember(member2);
+        container.store();
+        container.deleteMember(1);
+        container.load();
+
+        List<Member> currentList = container.getCurrentList();
+        assertEquals(2, currentList.size());
+        assertTrue(currentList.contains(member1));
+        assertTrue(currentList.contains(member2));
+    }
+
+
+
+
+}
