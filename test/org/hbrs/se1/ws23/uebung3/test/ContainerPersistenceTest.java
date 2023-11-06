2fa7be51d4f012fd04e6ecda65440ae11307f461
Mark.Sh <shkarin27@mail.ru>
2023-11-06 19:25:03 +0100

    testRoundTrip()-Methode gefixt und testStrategyNotSet()-Methode geändert

--- test/org/hbrs/se1/ws23/uebung3/test/ContainerPersistenceTest.java
+++ test/org/hbrs/se1/ws23/uebung3/test/ContainerPersistenceTest.java
@@ -12,15 +12,14 @@ import java.util.List;
 
 
 public class ContainerPersistenceTest {
-    private final Container container = new Container();
+    private final Container container = new SingletonContainer();
     private final PersistenceStrategyStream<Member> psS = new PersistenceStrategyStream<>();
     private final PersistenceStrategyMongoDB<Member> psMongoDB = new PersistenceStrategyMongoDB<>();
 
     @Test
-    void testStrategySet() {
-        container.setPersistenceStrategy(psS);
-        assertThrows(PersistenceException.class, () -> container.store());
-        assertThrows(PersistenceException.class, () -> container.load());
+    void testStrategyNotSet() {
+        assertThrows(NullPointerException.class, () -> container.store());
+        assertThrows(NullPointerException.class, () -> container.load());
     }
 
     @Test
@@ -43,19 +42,28 @@ public class ContainerPersistenceTest {
         Member member1 = new ConcreteMember(1);
         Member member2 = new ConcreteMember(2);
 
+        psS.setLocation("C:\\Users\\Mark\\Desktop\\Downloads\\1.pdf");
+        container.setPersistenceStrategy(psS);
+
         container.addMember(member1);
         container.addMember(member2);
+
         container.store();
         container.deleteMember(1);
-        container.load();
 
         List<Member> currentList = container.getCurrentList();
-        assertEquals(2, currentList.size());
-        assertTrue(currentList.contains(member1));
+
+        assertEquals(1, currentList.size());
+        assertFalse(currentList.contains(member1));
         assertTrue(currentList.contains(member2));
+
+        container.load();
+
+        List<Member> newList = container.getCurrentList();
+        Member backMember1 = newList.get(0);
+        Member backMember2 = newList.get(1);
+        assertEquals(member1.getID().intValue(), backMember1.getID().intValue());
+        assertEquals(member2.getID().intValue(), backMember2.getID().intValue());
+
     }
-
-
-
-
 }
