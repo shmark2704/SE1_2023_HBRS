052ae73d0c49919776b5e7bfa1bcdf2dc09fe15a
Mark.Sh <shkarin27@mail.ru>
2023-11-08 14:23:39 +0100

    Container-Objekt richtig erzeugt nach Singleton Prinzip

--- src/org/hbrs/se1/ws23/uebung2/Client.java
+++ src/org/hbrs/se1/ws23/uebung2/Client.java
@@ -3,7 +3,7 @@ package org.hbrs.se1.ws23.uebung2;
 public class Client {
 
     public static void main(String[] args) {
-        Container container = new Container();
+        Container container = Container.getInstance();
         MemberView memberView = new MemberView();
     }
 }
