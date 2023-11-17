34b200a8bf4a27c661b14ae533ff40b41e06b9de
Mark.Sh <shkarin27@mail.ru>
2023-11-17 15:10:37 +0100

    Util.java implementiert

--- src/org/hbrs/se1/ws23/uebung4/prototype/control/Util.java
+++ src/org/hbrs/se1/ws23/uebung4/prototype/control/Util.java
@@ -1,5 +1,25 @@
 package org.hbrs.se1.ws23.uebung4.prototype.control;
 
-public class Util {
+import org.hbrs.se1.ws23.uebung4.prototype.model.Container;
 
+import java.util.Collections;
+import java.util.List;
+
+public class Util {
+    /**
+     * Liefert eine bestimmte UserStory zurück
+     * @param id
+     * @return
+     */
+
+    private List<UserStory> list = Container.getInstance().getCurrentList();
+
+    private UserStory getUserStory(int id) {
+        for ( UserStory userStory : list) {
+            if (id == userStory.getId() ){
+                return userStory;
+            }
+        }
+        return null;
+    }
 }
