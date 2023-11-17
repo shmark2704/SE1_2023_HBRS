5e864d6db674f679418404e0d1f9f455a79658ae
Mark.Sh <shkarin27@mail.ru>
2023-11-17 13:45:48 +0100

    UserStoryView.java und Util.java erstellt

---
+++ src/org/hbrs/se1/ws23/uebung4/prototype/control/Util.java
@@ -0,0 +1,5 @@
+package org.hbrs.se1.ws23.uebung4.prototype.control;
+
+public class Util {
+
+}
---
+++ src/org/hbrs/se1/ws23/uebung4/prototype/view/UserStoryView.java
@@ -0,0 +1,7 @@
+package org.hbrs.se1.ws23.uebung4.prototype.view;
+
+public class UserStoryView {
+    public void dump() {
+
+    }
+}
