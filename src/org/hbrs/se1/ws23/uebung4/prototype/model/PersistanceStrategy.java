63b9cd3dbaa1ad670a74c9912c0810a67638d2d3
Mark.Sh <shkarin27@mail.ru>
2023-11-21 20:48:43 +0100

    Exceptions bei openConnection() und closeConnection durch throws ergänzt

--- src/org/hbrs/se1/ws23/uebung4/prototype/model/PersistanceStrategy.java
+++ src/org/hbrs/se1/ws23/uebung4/prototype/model/PersistanceStrategy.java
@@ -1,13 +1,14 @@
 package org.hbrs.se1.ws23.uebung4.prototype.model;
 
+import org.hbrs.se1.ws23.uebung3.persistence.PersistenceException;
 import org.hbrs.se1.ws23.uebung4.prototype.model.exception.ContainerException;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface PersistanceStrategy<UserStory> {
-        public void openConnection() throws IOException;
-        public void closeConnection() throws IOException;
+        public void openConnection() throws IOException, PersistenceException;
+        public void closeConnection() throws IOException, PersistenceException;
         public void save(List<UserStory> userStories) throws IOException, ContainerException;
         public List<UserStory> load() throws  IOException, ClassNotFoundException;
 }
