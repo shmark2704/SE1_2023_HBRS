abbcdb875acc7109aca8f698c4cde563b1415f0b
Mark.Sh <shkarin27@mail.ru>
2024-01-09 18:50:51 +0100

    ReiseAnbieter-Klasse implemntiert

---
+++ src/org/hbrs/se1/ws23/uebung8/ReiseAnbieter.java
@@ -0,0 +1,9 @@
+package org.hbrs.se1.ws23.uebung8;
+
+public class ReiseAnbieter {
+    private QueryResult result;
+
+    public QueryResult executeQuery(QueryObject obj) {
+        return null;
+    }
+}
